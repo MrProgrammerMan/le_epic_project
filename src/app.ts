e257501060c4ec8c7c5338c6979b98ee0b56dec5
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2025-09-22 09:24:11 +0200

    Setup react (#4)
    * chore: add frontend react app
    
    * feat(frontend): set build output path to accessibile dir for backend
    
    * chore(gitignore): add static
    
    * chore(dependencies): add fastify and fastify/static
    
    * feat(static serving): add basic fastify setup to serve static files

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 console.log("TypeScript Runs");
+
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fastify = Fastify({
+    logger: true,
+});
+
+fastify.register(fastifyStatic, {
+    root: join(__dirname, "..", "static"),
+});
+
+try {
+    await fastify.listen({ port: 3000 });
+    console.log("Server running at http://localhost:3000");
+} catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+}
