48feb3467808db4859b1df8cd6fde43a36a67804
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-01 07:31:57 +0200

    feat!(unfinished): move postgres to flake with services-flake

--- flake.nix
+++ flake.nix
@@ -1,57 +1,40 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    };
-    outputs = { self, nixpkgs, ... }:let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-        devShells.${system}.default = 
-        let
+  description = "My project with postgres service";
 
-            # Provides a script that copies required files to ~/
-            podmanSetupScript =
-            let
-                registriesConf = pkgs.writeText "registries.conf" ''
-                    [registries.search]
-                    registries = ['docker.io']
-                    [registries.block]
-                    registries = []
-                '';
-            in pkgs.writeScript "podman-setup" ''
-                #!${pkgs.runtimeShell}
-                # Dont overwrite customised configuration
-                if ! test -f ~/.config/containers/policy.json; then
-                    install -Dm555 ${pkgs.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-                fi
-                if ! test -f ~/.config/containers/registries.conf; then
-                    install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-                fi
-            '';
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+    systems.url = "github:nix-systems/default";
+  };
 
-            # Provides a fake "docker" binary mapping to podman
-            dockerCompat = pkgs.runCommandNoCC "docker-podman-compat" {} ''
-                mkdir -p $out/bin
-                ln -s ${pkgs.podman}/bin/podman $out/bin/docker
-            '';
-        in pkgs.mkShell {
-            packages = with pkgs; [
-                nodejs
-                dockerCompat
-                pkgs.podman  # Docker compat
-                pkgs.runc  # Container runtime
-                pkgs.conmon  # Container runtime monitor
-                pkgs.skopeo  # Interact with container registry
-                pkgs.slirp4netns  # User-mode networking for unprivileged namespaces
-                pkgs.fuse-overlayfs  # CoW for images, much faster than default vfs
-                pkgs.podman-compose
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.process-compose-flake.flakeModule ];
+
+      perSystem = { config, pkgs, ... }: {
+        # Define a process-compose environment
+        process-compose.default = { config, ... }: {
+          imports = [ inputs.services-flake.processComposeModules.default ];
+
+          services.postgres."pg1" = {
+            enable = true;
+            initialDatabases = [
+              { name = "db"; }
             ];
-            shellHook = ''
-                # Install required configuration
-                ${podmanSetupScript}
-
-                trap 'echo "Shutting down containers..."; podman-compose down' EXIT
-            '';
+          };
         };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.process-compose.default.services.outputs.devShell
+          ];
+          packages = with pkgs; [
+            nodejs
+          ];
+        };
+      };
     };
-}
+  }
