220cbd92b2279d3f281e358f351a62b9b4996599
= <=>
2025-10-02 14:49:31 +0200

    refactor(ts): make typescript happy with explicit any

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,6 +1,6 @@
 import Fastify from "fastify";
-
-export const errorHandler = function (error, request, reply) {
+// NOTE: change to something other than 'any'??
+export const errorHandler = function (error: any, request: any, reply: any) {
     if (error.validation) {
         reply.status(422).send(new Error('validation failed')) // bruke denne til å redirecte brukere tilbake til andre sider ved errorer.
     }
