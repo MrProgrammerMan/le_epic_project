8ddc2dbe420246e7dd1f73e3521e2de72685a4e2
= <=>
2025-10-02 14:55:16 +0200

    feat(db): add database connection object

---
+++ src/db/database.ts
@@ -0,0 +1,21 @@
+import { DB } from './types.js' // this is the Database interface we defined earlier
+import { Pool } from 'pg'
+import { Kysely, PostgresDialect } from 'kysely'
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    user: process.env.POSTGRES_USER,
+    port: Number(process.env.POSTGRES_PORT),
+    max: 10,
+  })
+})
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely 
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
+// to communicate with your database.
+export const db = new Kysely<DB>({
+  dialect,
+})
