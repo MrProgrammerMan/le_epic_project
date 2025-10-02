3e561afcc236033e5bbd0748aba4cafe595ac05a
= <=>
2025-10-02 15:04:14 +0200

    feat(course): add simple find routes for Course

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,44 +1,17 @@
 import Fastify from "fastify";
 import type { FastifyPluginAsync } from "fastify";
-import { courseGetSchema } from "../schema.ts";
-import type { courseObj } from "../interface/courseInterface.ts";
+import { findAllCourses, findCourseByCode } from "../repositories/courseRepository.js";
 
 const courseRoutes: FastifyPluginAsync = async (fastify, options) => {
-    /*
-    Parameter: 
-    - kode: string -> Fagkoden til kurs, feks DATA1200
-    
-    */
-    fastify.get("/course/:fagkode",
-        {schema: {
-            params: courseGetSchema
-        },
-    },
-    
-        function (request, reply) {
-            const parameters = request.params as { fagkode: string }
-            let kode = parameters.fagkode;
-            reply.send(kode);
-    }); 
-    
-    // trenger mer info om database koblingene og hvordan vi skal returnere dataen.
-    // men endpointsene er klare til å bli utviklet videre.
-    
-    fastify.get("/course/:fagkode/wiki", function (request, reply) {
+    fastify.get("/course/:fagkode", function (request, reply) {
         const parameters = request.params as { fagkode: string }
-        let kode = parameters.fagkode;
-        reply.send("Velkommen til wikisiden til " + kode);
+        const kode = parameters.fagkode;
+        reply.send(findCourseByCode(kode));
     });
 
-    fastify.get("/course", function (request, reply) {
-        // tester kobling. tenker hjemmeside til hvert fag her.
-        reply.send("test");
+    fastify.get("/courses", function (request, reply) {
+        reply.send(findAllCourses());
     });
-    fastify.get("/list", function (request, reply) {
-        // venter på database kobling
-        reply.send("returner liste over kurs");
-    });
-
 }
 
 export default courseRoutes;
