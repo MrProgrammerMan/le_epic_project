f63aafeaf410b01776481f96dd824fa4bb03ebfb
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-04 16:36:02 +0200

    feat(course): implement kysely types

--- src/repositories/courseRepository.ts
+++ src/repositories/courseRepository.ts
@@ -1,13 +1,19 @@
 import { db } from '../db/database.js';
+import { Insertable, Selectable } from 'kysely';
+import { DB } from '../db/types.js';
 
-export async function findCourseByCode(code: string) {
+type Course = Selectable<DB['course']>;
+type NewCourse = Insertable<DB['course']>;
+
+
+export async function findCourseByCode(code: string): Promise<Course | undefined> {
   return await db.selectFrom('course')
     .where('code', '=', code)
     .selectAll()
     .executeTakeFirst();
 }
 
-export async function findAllCourses() {
+export async function findAllCourses(): Promise<Course[]> {
   return await db.selectFrom('course')
     .selectAll()
     .execute();
