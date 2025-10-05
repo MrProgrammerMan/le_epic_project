1a42192ca603bc6947cdf005396ca24a981b54a5
ElimG05 <elghe0590@oslomet.no>
2025-10-05 18:34:06 +0200

    added subject info component with mock data

--- frontend/src/components/SubjectInfo.tsx
+++ frontend/src/components/SubjectInfo.tsx
@@ -2,9 +2,44 @@ import React from 'react';
 
 const SubjectInfo: React.FC = () => {
   return (
-    <section>
-      <h1>Skriv noe mer..</h1>
-    </section>
+    <div>
+      <div>
+        <h2>TOTAL SUBJECTS</h2>
+        <div>Total subjects: ? results</div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Discrete Mathematics</h3>
+          <p>DAPE1300</p>
+          <div></div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Programming</h3>
+          <p>DAPE1400</p>
+          <div></div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Technology and Society for Programmers</h3>
+          <p>DATA1100</p>
+          <div></div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Inclusive Web Design</h3>
+          <p>DATA1200</p>
+          <div></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
