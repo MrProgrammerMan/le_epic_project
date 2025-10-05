f08c815cbd08b98a4d7688dd0d92c0e676fa746c
ElimG05 <elghe0590@oslomet.no>
2025-10-05 19:32:13 +0200

    added some styling

--- frontend/src/components/SubjectInfo.tsx
+++ frontend/src/components/SubjectInfo.tsx
@@ -2,44 +2,36 @@ import React from 'react';
 
 const SubjectInfo: React.FC = () => {
   return (
-    <div>
-      <div>
-        <h2>TOTAL SUBJECTS</h2>
-        <div>Total subjects: ? results</div>
+  <div className='mr-8'>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-lg font-semibold">TOTAL SUBJECTS</h1>
+        <div className="text-sm text-gray-500">Total subjects: ? results</div>
       </div>
-
-      <div>
-        <div>
-          <h3>Discrete Mathematics</h3>
-          <p>DAPE1300</p>
-          <div></div>
-        </div>
+      <div className="mb-5">
+          <div className="border border-gray-500 rounded p-5">
+            <h3 className="text-lg font-semibold">Discrete Mathematics</h3>
+            <p>DAPE1300</p>
+          </div>
       </div>
-
-      <div>
-        <div>
-          <h3>Programming</h3>
+      <div className="mb-5">
+        <div className="border border-gray-500 rounded p-5">
+          <h3 className="text-lg font-semibold">Programming</h3>
           <p>DAPE1400</p>
-          <div></div>
-        </div>
+          </div>
       </div>
-
-      <div>
-        <div>
-          <h3>Technology and Society for Programmers</h3>
+      <div className="mb-5">
+        <div className="border border-gray-500 rounded p-5">
+          <h3 className="text-lg font-semibold">Technology and Society for Programmers</h3>
           <p>DATA1100</p>
-          <div></div>
-        </div>
+          </div>
       </div>
-
-      <div>
-        <div>
-          <h3>Inclusive Web Design</h3>
+      <div className="mb-5">
+        <div className="border border-gray-500 rounded p-5">
+          <h3 className="text-lg font-semibold">Inclusive Web Design</h3>
           <p>DATA1200</p>
-          <div></div>
-        </div>
+          </div>
       </div>
-    </div>
+      </div>
   );
 };
 
